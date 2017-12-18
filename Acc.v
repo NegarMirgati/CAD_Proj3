777e139c5f70c487095aade30415aef6f40418c9
Negar Mirgati <negar.nbj96@yahoo.com>
2017-12-18 10:03:21 +0330

    add clear_acc

--- Acc.v
+++ Acc.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module Acc #(parameter N)(input clk, input rst, input en, input [N-1:0] in, output[N-1:0] out);
+module Acc #(parameter N)(input clk, input rst,input clear_acc, input en, input [N-1:0] in, output[N-1:0] out);
 
 reg[N-1:0] data;
 
@@ -8,7 +8,12 @@ always @(posedge clk, posedge rst) begin
 if(rst)
 	data<= {(N){1'b0}};
 
+else if(clear_acc)
+
+	data <= {{N}{1'b0}};
+
 else if(en)
+
 	data <= in;
 
 
