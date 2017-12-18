26b23b3d413a8061c62782719d0fe13643041516
Negar Mirgati <negar.nbj96@yahoo.com>
2017-12-18 18:36:11 +0330

    fix activation func bug

--- ActivationFunc.v
+++ ActivationFunc.v
@@ -4,7 +4,7 @@ module ActivationFunc #(parameter N)(input ready, input [N-1:0] in , output reg 
 always @(*) begin
 
     if(ready) begin
-        assign out = (in[0] != 1'b1) ? {(N){1'b0}} : in;
+        assign out = (in[1] != 1'b1) ? {(N){1'b0}} : in;
     end
 end
 
